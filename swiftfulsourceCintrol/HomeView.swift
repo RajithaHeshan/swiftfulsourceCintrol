4bce3a122e85c6c92c752c713a8181e0d4793e85
RajithaHeshan <rajithaheshan1998@gmail.com>
2025-07-24 17:36:03 +0530

    Home -Page updates

--- swiftfulsourceCintrol/HomeView.swift
+++ swiftfulsourceCintrol/HomeView.swift
@@ -3,6 +3,13 @@ import SwiftUI
 struct HomeView: View {
     var body: some View {
         Text("Hello, World!")
+        ZStack {
+        Text("Hello, World!")
+            
+        Text ("Screen 2!")
+        Text ("Screen 2!")
+            
+        }
     }
     
 }

7fb7df1ddf8d2dc4066dd4bd354e692022ab6225
RajithaHeshan <rajithaheshan1998@gmail.com>
2025-07-24 15:36:38 +0530

    Initial Commit

---
+++ swiftfulsourceCintrol/swiftfulsourceCintrolApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftfulsourceCintrolApp.swift
+//  swiftfulsourceCintrol
+//
+//  Created by Heshan Dunumala on 2025-07-24.
+//
+
+import SwiftUI
+
+@main
+struct swiftfulsourceCintrolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swiftfulsourceCintrolTests/swiftfulsourceCintrolTests.swift
@@ -0,0 +1,17 @@
+//
+//  swiftfulsourceCintrolTests.swift
+//  swiftfulsourceCintrolTests
+//
+//  Created by Heshan Dunumala on 2025-07-24.
+//
+
+import Testing
+@testable import swiftfulsourceCintrol
+
+struct swiftfulsourceCintrolTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

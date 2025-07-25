72927a2e0bcd213d32d73309a36e183b5002fcc6
RajithaHeshan <rajithaheshan1998@gmail.com>
2025-07-25 14:29:16 +0530

    add new text1

--- swiftfulsourceCintrol/HomeView.swift
+++ swiftfulsourceCintrol/HomeView.swift
@@ -1,12 +1,13 @@
 import SwiftUI
 
 struct HomeView: View {
-    @State private var title: String = ""
+    @State private var title: String = "Hello world"
     var body: some View {
          VStack{
                 Text ("Screen 2!")
                 Text ("Screen 2!")
                 Text ("Hi")
+                Text("Hi ALL Blacks")
             }
             
         }
